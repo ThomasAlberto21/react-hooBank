d1e665add5ffccf1f2824c5c71933192cbde9a34
ThomasAlberto21 <thomasalberto456@gmail.com>
2022-12-10 13:23:42 +0700

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,29 @@
+import Navbar from './Navbar';
+import Billing from './Billing';
+import Button from './Button'
+import CardDeal from './CardDeal';
+import Business from './Business';
+import Clients from './Clients';
+import CTA from './CTA';
+import GetStarted from './GetStarted'
+import FeedbackCard from './FeedbackCard'
+import Stats from './Stats';
+import Footer from './Footer';
+import Testimonials from './Testimonials';
+import Hero from './Hero';
+
+export {
+  Navbar,
+  Billing,
+  CardDeal,
+  Button,
+  Business,
+  FeedbackCard,
+  Clients,
+  CTA,
+  GetStarted,
+  Stats,
+  Footer,
+  Testimonials,
+  Hero,
+};
